1051092e21f894ecfc45028b051eef9ecf14ee6b
JT Vaughan <48608690+vaughanjt@users.noreply.github.com>
2026-09-20 21:15:55 -0500

    Milestone 2: add rhastra/__init__.py

---
+++ rhastra/__init__.py
@@ -0,0 +1,6 @@
+"""RHAstra: inverse-spectral experiments around the Riemann zeta function."""
+
+from .targets import riemann_von_mangoldt_smooth
+
+__all__ = ["riemann_von_mangoldt_smooth"]
+__version__ = "0.2.0"
